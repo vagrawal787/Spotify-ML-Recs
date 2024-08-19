571e95af173e24bbc7370b9bca631da79cac0c87
Varun Agrawal <varun@gryps.io>
2024-08-19 12:28:55 -0400

    sample change for testing pr linking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import './App.css';
+// import './App.css'; okay
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Playlist from "./Playlist";
